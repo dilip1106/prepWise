1c8cfab1095b0f48655659f680d7e9d0bfff199e
dilip1106 <dilipteli38@gmail.com>
2025-04-24 10:54:26 +0530

    Implemented the feedback

--- components/InterviewCard.tsx
+++ components/InterviewCard.tsx
@@ -1,21 +1,39 @@
 import React from 'react'
 import dayjs from 'dayjs'
 import Image from 'next/image';
-import { getRandomInterviewCover } from '@/lib/utils';
+import { cn, getRandomInterviewCover } from '@/lib/utils';
 import { Button } from './ui/button';
 import Link from 'next/link';
 import DisplayTechIcons from './DisplayTechIcons';
-const InterviewCard = ({interviewId, userId, role, type, techstack, createdAt}:InterviewCardProps) => {
+import { getFeedbackByInterviewId } from '@/lib/actions/general.action';
+const InterviewCard = async  ({interviewId, userId, role, type, techstack, createdAt}:InterviewCardProps) => {
   
-    const feedback = null as Feedback | null;
+    const feedback =
+    userId && interviewId
+      ? await getFeedbackByInterviewId({
+          interviewId,
+          userId,
+        })
+      : null;
     const normalizedType = /mix/gi.test(type) ? 'Mixed' : type;
     const formattedDate = dayjs(feedback?.createdAt || createdAt || Date.now()).format('MMM D, YYYY');
+    const badgeColor =
+    {
+      Behavioral: "bg-light-400",
+      Mixed: "bg-light-600",
+      Technical: "bg-light-800",
+    }[normalizedType] || "bg-light-600";
 
     return (
         <div className='card-border w-[360px] max-sm:w-full min-h-96'>
             <div className='card-interview'>
                 <div>
-                    <div className='absolute top-0 right-0 w-fit px-4 py-2 rounded-bl-lg bg-light-600'>
+                   <div
+                        className={cn(
+                        "absolute top-0 right-0 w-fit px-4 py-2 rounded-bl-lg",
+                        badgeColor
+                        )}
+                    > 
                         <p className='badge-text'>{normalizedType}</p>
                     </div>
 
